d086753f445216543a9418ec0a40be9557db90cf
Brice SCHUMACHER <briceschumacher21@gmail.com>
2025-02-08 23:59:05 +0100

    test : (EnvironmentUnitTest) Adding tests on configuration mechanism

--- NotoriousTest.UnitTests/SyncEnvironmentUnitTests.cs
+++ NotoriousTest.UnitTests/SyncEnvironmentUnitTests.cs
@@ -8,6 +8,7 @@ namespace NotoriousTest.UnitTests
 
     public class SyncEnvironmentUnitTests
     {
+        #region Unique Infrastructure Test Cases
         #region Unique Infrastructure Test Cases Setup
         static UniqueInfrastructureTestCasesInfrastructure _uniqueInfrastructureTestCasesInfrastructure;
 
@@ -85,7 +86,8 @@ namespace NotoriousTest.UnitTests
             A.CallTo(() => _uniqueInfrastructureTestCasesInfrastructure.Reset()).MustNotHaveHappened();
             A.CallTo(() => _uniqueInfrastructureTestCasesInfrastructure.Destroy()).MustHaveHappenedOnceExactly();
         }
-
+        #endregion
+        #region Multiple Infrastructure Test Cases
         #region Multiple Infrastructure Test Cases Setup
         static MultipleInfrastructureTestCasesInfrastructure1 _multipleInfrastructureTestCasesInfrastructure1;
         static MultipleInfrastructureTestCasesInfrastructure2 _multipleInfrastructureTestCasesInfrastructure2;
@@ -208,5 +210,156 @@ namespace NotoriousTest.UnitTests
                         .MustHaveHappenedOnceExactly()
                 );
         }
+        #endregion
+        #region Configuration Object Test Cases
+        #region Configuration Object Test Cases Setup
+        static ConfigurationObjectTestCasesInfrastructure1 _configurationObjectTestCasesInfrastructure1;
+        static ConfigurationObjectTestCasesInfrastructure2 _configurationObjectTestCasesInfrastructure2;
+
+        public class ConfigurationObjectTestCasesEnvironmentConfiguraton
+        {
+            public string Key1 { get; set; }
+            public string Key2 { get; set; }
+        }
+
+        public class ConfigurationObjectTestCasesEnvironment : ConfiguredEnvironment<ConfigurationObjectTestCasesEnvironmentConfiguraton>
+        {
+            public ConfigurationObjectTestCasesEnvironment()
+            {
+            }
+            public override void ConfigureEnvironment()
+            {
+                AddInfrastructure(_configurationObjectTestCasesInfrastructure1);
+                AddInfrastructure(_configurationObjectTestCasesInfrastructure2);
+            }
+        }
+
+        public class ConfigurationObjectTestCasesInfrastructure1 : ConfiguredInfrastructure<ConfigurationObjectTestCasesEnvironmentConfiguraton>
+        {
+            public ConfigurationObjectTestCasesInfrastructure1() : base(false)
+            {
+            }
+
+            public override void Destroy()
+            {
+            }
+
+            public override void Initialize()
+            {
+                Configuration.Key1 = "Infra1Key1";
+            }
+
+            public override void Reset()
+            {
+            }
+        }
+
+        public class ConfigurationObjectTestCasesInfrastructure2 : ConfiguredInfrastructure<ConfigurationObjectTestCasesEnvironmentConfiguraton>
+        {
+            public ConfigurationObjectTestCasesInfrastructure2() : base(false)
+            {
+            }
+
+            public override void Destroy()
+            {
+            }
+
+            public override void Initialize()
+            {
+                Configuration.Key2 = "Infra2Key2";
+            }
+
+            public override void Reset()
+            {
+            }
+        }
+
+
+        #endregion
+
+        [Fact]
+        public void InfrastructureWithinEnvironmentWithObjectConfiguration_Should_ProduceConfigurationCorrectly()
+        {
+            _configurationObjectTestCasesInfrastructure1 = new ConfigurationObjectTestCasesInfrastructure1();
+            _configurationObjectTestCasesInfrastructure2 = new ConfigurationObjectTestCasesInfrastructure2();
+
+            var environment = new ConfigurationObjectTestCasesEnvironment();
+            Assert.Equal("Infra1Key1", environment.EnvironmentConfiguration.Key1);
+            Assert.Equal("Infra2Key2", environment.EnvironmentConfiguration.Key2);
+        }
+
+        #endregion
+        #region Configuration Dictionary Test Cases
+        #region Configuration Dictionary Test Cases Setup
+        static ConfigurationDictionaryTestCasesInfrastructure1 _configurationDictionaryTestCasesInfrastructure1;
+        static ConfigurationDictionaryTestCasesInfrastructure2 _configurationDictionaryTestCasesInfrastructure2;
+
+        public class ConfigurationDictionaryTestCasesEnvironment : ConfiguredEnvironment
+        {
+            public ConfigurationDictionaryTestCasesEnvironment()
+            {
+            }
+            public override void ConfigureEnvironment()
+            {
+                AddInfrastructure(_configurationDictionaryTestCasesInfrastructure1);
+                AddInfrastructure(_configurationDictionaryTestCasesInfrastructure2);
+            }
+        }
+
+        public class ConfigurationDictionaryTestCasesInfrastructure1 : ConfiguredInfrastructure
+        {
+            public ConfigurationDictionaryTestCasesInfrastructure1() : base(false)
+            {
+            }
+
+            public override void Destroy()
+            {
+            }
+
+            public override void Initialize()
+            {
+                Configuration.Add("Key1", "Infra1Key1");
+            }
+
+            public override void Reset()
+            {
+            }
+        }
+
+        public class ConfigurationDictionaryTestCasesInfrastructure2 : ConfiguredInfrastructure
+        {
+            public ConfigurationDictionaryTestCasesInfrastructure2() : base(false)
+            {
+            }
+
+            public override void Destroy()
+            {
+            }
+
+            public override void Initialize()
+            {
+                Configuration.Add("Key2", "Infra2Key2");
+            }
+
+            public override void Reset()
+            {
+            }
+        }
+
+
+        #endregion
+
+        [Fact]
+        public void InfrastructureWithinEnvironmentWithDictionaryConfiguration_Should_ProduceConfigurationCorrectly()
+        {
+            _configurationDictionaryTestCasesInfrastructure1 = new ConfigurationDictionaryTestCasesInfrastructure1();
+            _configurationDictionaryTestCasesInfrastructure2 = new ConfigurationDictionaryTestCasesInfrastructure2();
+
+            var environment = new ConfigurationDictionaryTestCasesEnvironment();
+            Assert.Equal("Infra1Key1", environment.EnvironmentConfiguration["Key1"]);
+            Assert.Equal("Infra2Key2", environment.EnvironmentConfiguration["Key2"]);
+        }
+
+        #endregion
     }
 }
