a9468fcd7809e1c7f8f5b8933dadca7ca3d53d7e
Brice SCHUMACHER <briceschumacher21@gmail.com>
2025-02-09 23:28:10 +0100

    clean : (AsyncConfiguredEnvironment) Revert offtopic modification

--- NotoriousTest/Common/Environments/AsyncConfiguredEnvironment.cs
+++ NotoriousTest/Common/Environments/AsyncConfiguredEnvironment.cs
@@ -3,8 +3,27 @@ using NotoriousTest.Common.Infrastructures.Async;
 
 namespace NotoriousTest.Common.Environments
 {
-    public abstract class AsyncConfiguredEnvironment : AsyncConfiguredEnvironment<Dictionary<string, string>>
+    public abstract class AsyncConfiguredEnvironment : AsyncEnvironment
     {
+        public Dictionary<string, string> EnvironmentConfiguration { get; set; } = new();
+
+        public async override Task Initialize()
+        {
+            foreach (AsyncInfrastructure infra in Infrastructures.OrderBy(i => i.Order))
+            {
+                if (infra is AsyncConfiguredInfrastructure consumer)
+                {
+                    consumer.Configuration = EnvironmentConfiguration;
+                }
+
+                await infra.Initialize();
+
+                if (infra is AsyncConfiguredInfrastructure producer)
+                {
+                    EnvironmentConfiguration = producer.Configuration;
+                }
+            }
+        }
     }
 
     public abstract class AsyncConfiguredEnvironment<TConfig> : AsyncEnvironment where TConfig : class, new()
