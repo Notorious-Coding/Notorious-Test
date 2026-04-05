26499ab148ba17cd02b44a76de90bedc4e9788a3
Brice SCHUMACHER <briceschumacher21@gmail.com>
2026-04-05 23:03:39 +0200

    fix: Add NUnit integration

---
+++ Samples/NotoriousTests.InfrastructuresSamples.NUnit/Infrastructures/TestWebApplication.cs
@@ -0,0 +1,13 @@
+using NotoriousTest.Web.Applications;
+
+namespace NotoriousTests.InfrastructuresSamples.NUnit.Infrastructures
+{
+    /// <summary>
+    /// A web application will automatically consume configuration from the environment ! No more code needed.
+    /// But you can still override WebApplicationFactory methods if you need to.
+    /// </summary>
+    public class TestWebApplication : WebApplication<Program>
+    {
+
+    }
+}
