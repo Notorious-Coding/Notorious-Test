f187cd6ef3a40f78150f6c92c82a868dd9765f3b
Brice SCHUMACHER <briceschumacher21@gmail.com>
2026-04-24 14:19:07 +0200

    tech: Adding private assets for internal packages

--- Commands/publish-local-nugets.ps1
+++ Commands/publish-local-nugets.ps1
@@ -1,4 +1,4 @@
-# Clean NuGet cache
+﻿# Clean NuGet cache
 Write-Host "Cleaning NuGet cache..."
 Remove-Item -Recurse -Force "$env:USERPROFILE\.nuget\packages\notorioustest.core" -ErrorAction SilentlyContinue
 
@@ -8,7 +8,7 @@ dotnet publish ./DoggyDog/DoggyDog.csproj `
     -r win-x64 `
     --self-contained `
     -p:PublishSingleFile=true `
-    -o ./tools/win-x64
+    -o ./artifacts/doggydog/win-x64
 
 # Publish DoggyDog for Linux
 Write-Host "Publishing DoggyDog for linux-x64..."
@@ -16,9 +16,9 @@ dotnet publish ./DoggyDog/DoggyDog.csproj `
     -r linux-x64 `
     --self-contained `
     -p:PublishSingleFile=true `
-    -o ./tools/linux-x64
+    -o ./artifacts/doggydog/linux-x64
 
 # Pack NotoriousTest.Core
-Write-Host "Packing TestFrameworks..."
+Write-Host "Packing nugets..."
 dotnet pack ./NotoriousTest.slnx -o ./local-packages --version-suffix local
 Write-Host "Done!"
