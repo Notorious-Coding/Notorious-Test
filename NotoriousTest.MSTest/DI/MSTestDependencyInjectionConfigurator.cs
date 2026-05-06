757b8494fe523e6f50704c9bd992c9a6ec6c83bf
Brice SCHUMACHER <briceschumacher21@gmail.com>
2026-05-06 10:54:40 +0200

    fix: MSTest testcontext injection

---
+++ NotoriousTest.MSTest/DI/MSTestDependencyInjectionConfigurator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NotoriousTest.Core.DI;
+using NotoriousTest.Core.Logger;
+using NotoriousTest.MSTest.Logger;
+
+namespace NotoriousTest.MSTest.DI;
+
+public class MSTestDependencyInjectionConfigurator : IDependencyInjectionConfigurator
+{
+    internal static TestContext TestContext { get; set; }
+
+    public IServiceCollection ConfigureServices(IServiceCollection services) =>
+        services
+            .AddSingleton(TestContext)
+            .AddSingleton<ITestLogger, MSTestTestLogger>();
+}
--- NotoriousTest.MSTest/IntegrationTestBase.cs
+++ NotoriousTest.MSTest/IntegrationTestBase.cs
@@ -1,9 +1,12 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NotoriousTest.Core;
+using NotoriousTest.Core.DI;
 using NotoriousTest.Core.Environments;
+using NotoriousTest.MSTest.DI;
 
 namespace NotoriousTest.MSTest
 {
+    [InjectionConfigurator(typeof(MSTestDependencyInjectionConfigurator))]
     [TestClass]
     public abstract class IntegrationTestBase<T> : NotoriousTest.IntegrationTestBase<T> where T : EnvironmentBase
     {
@@ -16,7 +19,9 @@ namespace NotoriousTest.MSTest
         [ClassInitialize(InheritanceBehavior.BeforeEachDerivedClass)]
         public static async Task ClassInitializeAsync(TestContext testContext)
         {
-            _fixture = new Fixture<T>(Type.GetType(testContext.FullyQualifiedTestClassName));
+            var type = AppDomain.CurrentDomain.GetAssemblies().SelectMany(x => x.GetTypes()).First(x => x.FullName == testContext.FullyQualifiedTestClassName);
+            MSTestDependencyInjectionConfigurator.TestContext = testContext;
+            _fixture = new Fixture<T>(type);
             await _fixture.Environment.Initialize();
         }
 
