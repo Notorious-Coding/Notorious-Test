099b552670c885c848d243893d80e4538dddbf9d
Notorious Code <85607176+Notorious-Coding@users.noreply.github.com>
2026-09-06 15:39:57 +0200

    chore/upgrade dependencies and desactivate nuget audit on samples pro… (#69)
    * chore/upgrade dependencies and desactivate nuget audit on samples project
    
    * chore: Docker.DotNet.Enhanced was taking over Docker.DotNet
    
    * chore: Use microsoft testing platoform
    
    * fix: sqlite package wase using postgreid
    
    ---------
    
    Co-authored-by: SCHUMACHER Brice (ENGIE Renewables) <sv6744@engie.com>

--- NotoriousTest.Database/DockerDatabaseInfrastructure.cs
+++ NotoriousTest.Database/DockerDatabaseInfrastructure.cs
@@ -1,57 +1,47 @@
-﻿using Docker.DotNet;
-
+﻿using System.Data.Common;
+using Docker.DotNet;
+using Docker.DotNet.Models;
 using DotNet.Testcontainers.Containers;
-
 using NotoriousTest.Core;
 using NotoriousTest.Core.Infrastructures.Cleaner;
 using NotoriousTest.Core.Logger;
 using NotoriousTest.Core.Registry;
 using NotoriousTest.TestContainers;
 
-using System.Data.Common;
+namespace NotoriousTest.Database;
 
-namespace NotoriousTest.Database
+[Cleaner(typeof(DockerInfrastructureCleaner))]
+public abstract class
+    DockerDatabaseInfrastructure<TContainer, TOutputConfiguration> : DatabaseInfrastructureBase<TOutputConfiguration,
+    DockerMetadata> where TContainer : IDatabaseContainer
 {
-    [Cleaner(typeof(DockerInfrastructureCleaner))]
-    public abstract class DockerDatabaseInfrastructure<TContainer, TOutputConfiguration> : DatabaseInfrastructureBase<TOutputConfiguration, DockerMetadata> where TContainer : IDatabaseContainer
+    protected DockerDatabaseInfrastructure(EnvironmentId contextId, ITestLogger logger, IRegistry registry) : base(
+        contextId, logger, registry)
     {
-        protected DockerDatabaseInfrastructure(EnvironmentId contextId, ITestLogger logger, IRegistry registry) : base(contextId, logger, registry)
-        {
-            if (Environment.GetEnvironmentVariable("TESTCONTAINERS_RYUK_DISABLED") != "true")
-                Environment.SetEnvironmentVariable("TESTCONTAINERS_RYUK_DISABLED", "true");
-        }
+        if (Environment.GetEnvironmentVariable("TESTCONTAINERS_RYUK_DISABLED") != "true")
+            Environment.SetEnvironmentVariable("TESTCONTAINERS_RYUK_DISABLED", "true");
+    }
 
-        public TContainer Container { get; init; }
+    public TContainer Container { get; init; }
 
-        public override async Task Initialize()
-        {
-            await Container.StartAsync();
-            Metadata = new DockerMetadata
-            {
-                ContainerID = Container.Id,
-                ContainerName = Container.Name,
-            };
-            await Register();
-            await base.Initialize();
-        }
+    public override async Task Initialize()
+    {
+        await Container.StartAsync();
+        Metadata = new DockerMetadata { ContainerID = Container.Id, ContainerName = Container.Name };
+        await Register();
+        await base.Initialize();
+    }
 
-        public override async Task Destroy()
-        {
-            var client = new DockerClientConfiguration().CreateClient();
-            await client.Containers.RemoveContainerAsync(Container.Id, new Docker.DotNet.Models.ContainerRemoveParameters()
-            {
-                Force = true
-            });
-        }
+    public override async Task Destroy()
+    {
+        DockerClient client = new DockerClientBuilder().Build();
+        await client.Containers.RemoveContainerAsync(Container.Id, new ContainerRemoveParameters { Force = true });
+    }
 
-        public override string GetServerConnectionString()
-        {
-            return Container.GetConnectionString();
-        }
+    public override string GetServerConnectionString() => Container.GetConnectionString();
 
-        protected override async Task DropDatabase(DbConnection connection)
-        {
-            // Dropping the database is unnecessary since the Docker container is destroyed with it.
-        }
+    protected override async Task DropDatabase(DbConnection connection)
+    {
+        // Dropping the database is unnecessary since the Docker container is destroyed with it.
     }
 }
